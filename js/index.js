c32c5f7c3c19b408390013dcc624a35a164ae847
Ryan McCrory <ryanmccrory83@gmail.com>
2019-03-03 10:00:48 -0600

    added grid, elements and styling to project page. need to render loren ipsum using javascript, as well as implement svg logo and bootstrap without using cdn

---
+++ js/index.js
@@ -0,0 +1,12 @@
+var message = 'Resilient do-gooder support co-create greenwashing paradigm, social enterprise storytelling. But NGO preliminary thinking segmentation invest program area. Resilient then technology sustainable optimism inclusive. Think tank, mobilize social enterprise move the needle unprecedented challenge framework progress. Entrepreneur, progress low-hanging fruit youth.'
+
+function displayMessage() {
+    const serviceMessage = document.querySelector('.service-p').textContent('message');
+    serviceMessage.innerHTML = message;
+}
+
+//function createServices() {
+//    for (let i = 0; i <= 3; i++) {
+//        const service = document.createElement('section').classList('services')
+//    }
+//}
