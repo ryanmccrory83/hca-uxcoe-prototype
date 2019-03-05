3762dcfdc2ee1d9f772997d696d6efd51fa73ffe
Ryan McCrory <ryanmccrory83@gmail.com>
2019-03-05 13:50:49 -0600

    added sass dependency, added commentary and fixed bootstrap scripts, completed bootstrap modal

--- js/index.js
+++ js/index.js
@@ -3,3 +3,16 @@ function injectIpsum() {
     ipsum.innerText = 'Resilient do-gooder support co-create greenwashing paradigm, social enterprise storytelling. But NGO preliminary thinking segmentation invest program area. Resilient then technology sustainable optimism inclusive. Think tank, mobilize social enterprise move the needle unprecedented challenge framework progress. Entrepreneur, progress low-hanging fruit youth.';
 }
 injectIpsum();
+
+var conHeight = $(".container").height();
+var imgHeight = $(".container img").height();
+var gap = (imgHeight - conHeight) / 2;
+$(".container img").css("margin-top", -gap);
+
+
+// if (jQuery) {
+//     console.log('yes');
+//     $(window).on(load).ready(function (e) {
+//         $('#contactModal').trigger('click');
+//     });
+// }
